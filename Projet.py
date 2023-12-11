87504e210fba5420fe5b37af089b37ab66ef9fe7
QuentinGuerinot <153364174+QuentinGuerinot@users.noreply.github.com>
2023-12-11 12:19:41 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -6,13 +6,19 @@ import time
 ##Classe des résultats d'optimisation
 class Results():
     def __init__(soc,ta,tb):
-        self.soc = soc
+        self.soc_init = soc_init
+        self.soc_final = soc_final
         self.ta = ta
-        self.td= tb
+        self.td = tb 
         
 def run_opti(config):
 
     ##paramètres (tiré d'un autre fichier, directement ici dans un premier temps):
+
+    Ck=1 #prix 
+    Pmax=140 #puissance max borne
+    N=100 #intervalle de temps 
+
 
 
     opti = Opti() # Optimization problem
@@ -23,14 +29,22 @@ def run_opti(config):
     X = opti.variable(1,N) 
 
     #Formule a minimiser 
-    J = Ck
+    for k in range(N):
+        J = Ck[:,k]*P[:,k]
+    J = (ta-tb)*J
     opti.minimize(J)
 
+    for k in range(N):
+        opti.subject_to(X[:,k+1]==X[:,k]+(ta-tb)*P[:,k]) # close the gaps    
+
     # ---- states constraints -----------
-    opti.subject_to(opti.bounded(soc_final,soc,xmax))
+    opti.subject_to(opti.bounded(0,Xk,Xmax))
+    opti.subject_to(opti.bounded(0,Pi,Pmax))
+    opti.subject_to(opti.bounded(soc_final,Xdj,Xmax))
 
         # ---- initial values for solver ---
-    opti.set_initial(tf, 20*60)
+    opti.set_initial(X, soc_init)
+    
     
     # ---- solve NLP              ------
     opti.solver("ipopt") # set numerical backend
