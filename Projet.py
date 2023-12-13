87f7537850ebf47058c1886db62ba9ccea6026c6
QuentinGuerinot <153364174+QuentinGuerinot@users.noreply.github.com>
2023-12-13 15:30:39 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -4,8 +4,7 @@ import math
 import time
 
 import matplotlib.pyplot as plt
-from   matplotlib.figure import Figure
-from   matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from pylab  import step, figure, show, spy
 
 ##Classe des résultats d'optimisation
 class Results():
@@ -14,57 +13,56 @@ class Results():
         self.soc_final = soc_final
         self.ta = ta
         self.td = td
-        
+        self.capa = Xmax
 
+#temporaire (plus tard sera recupéré)
+Xmax=50 #Capa batterrie
+soc_init=0 #% capa init
+soc_final=100 #% capa finale
+ta=0 # temps d'arrivé h
+td=10# temps final h
 
-#temporaire
-kwh=50
-soc_init=50
-soc_final=100
-kwh_init=kwh*soc_init/100
-kwh_final=kwh*soc_final/100
-Xmax=kwh
-ta=0
-td=24
+kwh_init=Xmax*soc_init/100 
+kwh_final=Xmax*soc_final/100
 
 ##paramètres (tiré d'un autre fichier, directement ici dans un premier temps):
-Ck_temp=np.array([100,1,1,1,1,100,1,1,1,1,1,1,1,100,1,1,1,1,1,1,1,1,1,1]) #prix 
+Ck=np.array([100,1,1,1,1,100,1,1,1,1,1,1,1,100,1,1,1,1,1000,1,1,1,1,1]) #vecteur de prix selon l'heure
 Pmax=140 #puissance max borne
-N=24 #intervalle de temps heures
-#beta=N/(ta-td)
-Dt=(td-ta)
+Dt=(td-ta) #différence de temps (utile pour le pas d'opti)
+
+t = np.linspace(ta,td,Dt+1) #vecteur temps pour le plot
 
 opti = Opti() # Optimization problem
 
 #Variables :
-P = opti.variable(1,Dt) 
-X = opti.variable(1,Dt) 
-Ck = opti.parameter(1,N)
-
-opti.set_value(Ck,Ck_temp)
+P = opti.variable(1,Dt+1) 
+X = opti.variable(1,Dt+1) 
 
 #Formule a minimiser
+
 J=0 
-for k in range(Dt):
+for k in range(Dt+1):
     J =  J + P[:,k] * Ck[k+ta] 
 J = Dt*J
+
 opti.minimize(J)
 
-for k in range(Dt-1):
+for k in range(Dt):
     opti.subject_to(X[:,k+1]==X[:,k]+P[:,k]) # close the gaps    
 
 # ---- states constraints -----------
 opti.subject_to(opti.bounded(0,X[:,:],Xmax))
 opti.subject_to(opti.bounded(0,P[:,:],Pmax))
-opti.subject_to(opti.bounded(kwh_final,X[:,Dt-1],Xmax))
+opti.subject_to(opti.bounded(kwh_final,X[:,Dt],Xmax))
 
-opti.subject_to(X[:,0]== kwh_init)
-#opti.subject_to(P[:,0]== 0)
+
 
     # ---- initial values for solver ---
 #opti.set_initial(X[:,0], kwh_init)
-#opti.set_initial(P[:,:], 0)
+opti.set_initial(P[:,:], 0)
 
+opti.subject_to(X[:,0]== kwh_init)
+#opti.subject_to(P[:,0]== 0)
 
 # ---- solve NLP              ------
 opti.solver("ipopt") # set numerical backend
@@ -73,25 +71,25 @@ tic = time.process_time()
 sol = opti.solve()   # actual solve
 toc = time.process_time()
 
-print(sol.value(Ck))
 print(sol.value(P))
-print(sol.value(J))
 print(sol.value(X))
 
-#Figure()
-#plt.plot(N,P)
-#plt.xlabel('Temps [min]')
-#plt.ylabel('P')
-#plt.title('Puissance vs time')
-#plt.grid()
 
-#Figure()
-#plt.plot(N,X)
-#plt.xlabel('Temps [min]')
-#plt.ylabel('X')
-#plt.title('Soc vs time')
-#plt.grid()
+figure()
+plt.plot(t,sol.value(P))
+plt.xlabel('Temps [heures]')
+plt.ylabel('P (kW)')
+plt.title('Puissance vs time')
+plt.grid()
 
+figure()
+plt.plot(t,sol.value(X))
+plt.xlabel('Temps [heures]')
+plt.ylabel('X (kWh)')
+plt.title('Soc vs time')
+plt.grid()
+
+plt.show()
 
 # put optimization results in a class
 #res = Results(sol.value(P)
