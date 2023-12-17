1605155ae79ed6f790de695f0cc4a17e1d6f579c
QuentinGuerinot <153364174+QuentinGuerinot@users.noreply.github.com>
2023-12-17 16:10:20 +0100

    Update Projet V1.py

--- Projet V1.py
+++ Projet V1.py
@@ -6,19 +6,11 @@ import time
 import matplotlib.pyplot as plt
 from pylab  import step, figure, show, spy
 
-##Classe des résultats d'optimisation
-class Results():
-    def __init__(self,P,X,t):
-        self.P = P
-        self.X = X
-        self.t = t
 
-#def run_opti():
-
-#temporaire (plus tard sera recupéré)
-Xmax=50 #Capa batterrie
-soc_init=0 #% capa init
-soc_final=100 #% capa finale
+#Variables du problème
+Xmax=50 #Capa batterie kWh
+soc_init=0 #capa init %
+soc_final=100 # capa finale %
 ta=0 # temps d'arrivé h
 td=19# temps final h
 Pmax=140 #puissance max borne
@@ -28,22 +20,15 @@ kwh_final=Xmax*soc_final/100
 
 ##paramètres (tiré d'un autre fichier, directement ici dans un premier temps):
 Ck=np.array([0.045,0.042,0.035,0.035,0.035,0.041,0.045,0.05,0.052,0.052,0.06,0.1,0.06,0.095,0.1,0.098,0.095,0.05,0.06,0.095,0.05,0.05,0.048,0.045]) #vecteur de prix selon l'heure
-Pw=np.array([0,0,35,50,70,45,25,20,0,0,15,0,45,30,0,0,30,40,30,5,30,30,40,10])
 Dt=(int(td*60)-int(ta*60)) #différence de temps (utile pour le pas d'opti)
-#Dt=td-ta
 Ck_temp = np.linspace(0,Dt,Dt+1)
-
-
 
 for k in range(Dt+1):
     Ck_temp[k]=Ck[int((k+int(ta*60))/60)]
 
-
-t_heures = np.linspace(0,24,24)
 t = np.linspace(ta,td,Dt+1) #vecteur temps pour le plot
 
 opti = Opti() # Optimization problem
-
 
 #Variables :
 P = opti.variable(1,Dt+1) 
@@ -55,18 +40,12 @@ J=0
 for k in range(Dt+1):
     J =  J + P[:,k] * Ck_temp[k]
 
-#for k in range(Dt+1):
-#    J =  J + P[:,k] * Ck[k]
-
 J = Dt*J
 
 opti.minimize(J)
 
 for k in range(Dt):
-    opti.subject_to(X[:,k+1]==X[:,k]+1/60*P[:,k]) # close the gaps
-
-#for k in range(Dt):
-#    opti.subject_to(X[:,k+1]==X[:,k]+P[:,k]) # close the gaps
+    opti.subject_to(X[:,k+1]==X[:,k]+1/60*P[:,k]) 
 
 # ---- states constraints -----------
 opti.subject_to(opti.bounded(0,X[:,:],Xmax))
@@ -87,16 +66,6 @@ opti.solver("ipopt") # set numerical backend
 tic = time.process_time()
 sol = opti.solve()   # actual solve
 toc = time.process_time()
-
-#put optimization results in a class
-#res = Results(sol.value(P),
-                #sol.value(X),
-                #t  
-                #)
-    
-#return res
-
-
 
 figure()
 plt.plot(t,sol.value(P))
