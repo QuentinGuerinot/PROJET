b656454acd7f832b19c888e0232fa287c82cbda0
QuentinGuerinot <153364174+QuentinGuerinot@users.noreply.github.com>
2023-12-17 11:41:54 +0100

    Update Projet V1.py

--- Projet V1.py
+++ Projet V1.py
@@ -17,9 +17,9 @@ class Results():
 
 #temporaire (plus tard sera recupéré)
 Xmax=50 #Capa batterrie
-soc_init=50 #% capa init
+soc_init=0 #% capa init
 soc_final=100 #% capa finale
-ta=15 # temps d'arrivé h
+ta=0 # temps d'arrivé h
 td=19# temps final h
 Pmax=140 #puissance max borne
 
@@ -28,12 +28,22 @@ kwh_final=Xmax*soc_final/100
 
 ##paramètres (tiré d'un autre fichier, directement ici dans un premier temps):
 Ck=np.array([0.045,0.042,0.035,0.035,0.035,0.041,0.045,0.05,0.052,0.052,0.06,0.1,0.06,0.095,0.1,0.098,0.095,0.05,0.06,0.095,0.05,0.05,0.048,0.045]) #vecteur de prix selon l'heure
+Pw=np.array([0,0,35,50,70,45,25,20,0,0,15,0,45,30,0,0,30,40,30,5,30,30,40,10])
 Dt=(int(td*60)-int(ta*60)) #différence de temps (utile pour le pas d'opti)
+#Dt=td-ta
+Ck_temp = np.linspace(0,Dt,Dt+1)
+
+
+
+for k in range(Dt+1):
+    Ck_temp[k]=Ck[int((k+int(ta*60))/60)]
+
 
 t_heures = np.linspace(0,24,24)
 t = np.linspace(ta,td,Dt+1) #vecteur temps pour le plot
 
 opti = Opti() # Optimization problem
+
 
 #Variables :
 P = opti.variable(1,Dt+1) 
@@ -43,14 +53,20 @@ X = opti.variable(1,Dt+1)
 
 J=0 
 for k in range(Dt+1):
-    J =  J + P[:,k] * Ck[int((k+int(ta*60))/60)]
-    print(Ck[int((k+int(ta*60))/60)])
+    J =  J + P[:,k] * Ck_temp[k]
+
+#for k in range(Dt+1):
+#    J =  J + P[:,k] * Ck[k]
+
 J = Dt*J
 
 opti.minimize(J)
 
 for k in range(Dt):
     opti.subject_to(X[:,k+1]==X[:,k]+1/60*P[:,k]) # close the gaps
+
+#for k in range(Dt):
+#    opti.subject_to(X[:,k+1]==X[:,k]+P[:,k]) # close the gaps
 
 # ---- states constraints -----------
 opti.subject_to(opti.bounded(0,X[:,:],Xmax))
@@ -59,7 +75,7 @@ opti.subject_to(opti.bounded(kwh_final,X[:,Dt],Xmax))
 
 
 # ---- initial values for solver ---
-#opti.set_initial(X[:,0], kwh_init)
+opti.set_initial(X[:,0], kwh_init)
 opti.set_initial(P[:,:], 0)
 
 opti.subject_to(X[:,0]== kwh_init)
@@ -97,7 +113,7 @@ plt.title('Soc vs time')
 plt.grid()
 
 figure()
-plt.plot(t_heures,Ck)
+plt.plot(t,Ck_temp)
 plt.xlabel('Temps [heures]')
 plt.ylabel('Prix (€)')
 plt.title('Prix du kWh')
